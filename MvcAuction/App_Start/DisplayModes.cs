9c6cfcf86a33ed4ad3c9ed573179020e30d5c4a3
biswajitpanday <biswajitmailid@gmail.com>
2015-02-08 05:47:31 +0600

    First commit

---
+++ MvcAuction/App_Start/DisplayModes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Web;
+using System.Web.WebPages;
+
+namespace MvcAuction.App_Start
+{
+    public class DisplayModes
+    {
+        public static void ConfigureDisplayModes()
+        {
+            //Register iPhone-specific Views.
+            DisplayModeProvider.Instance.Modes.Insert(0, new DefaultDisplayMode("iPhone")
+            {
+                ContextCondition = (ctx => ctx.Request.UserAgent.IndexOf(
+                    "iPhone", StringComparison.OrdinalIgnoreCase) >= 0)
+            });
+
+            //Register iPand-specific Views.
+            DisplayModeProvider.Instance.Modes.Insert(0, new DefaultDisplayMode("iPad")
+            {
+                ContextCondition = (ctx => ctx.Request.UserAgent.IndexOf(
+                    "iPad", StringComparison.OrdinalIgnoreCase) >=0)
+            });
+
+        }
+    }
+}
---
+++ MvcAuction/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Web;
+using System.Web.Caching;
+using System.Web.Mvc;
+using System.Web.WebPages;
+using MvcAuction.Models;
+
+namespace MvcAuction.Controllers
+{
+    [AllowAnonymous]
+    public class HomeController : Controller
+    {
+        [OutputCache(Duration = 5)]
+        public ActionResult Index()
+        {
+            ViewBag.Message = "This Page Was Rendered At " + DateTime.Now;
+
+            return View();
+        }
+
+        [OutputCache(Duration = 3600)]
+        public ActionResult CategoryNavigation()
+        {
+            var db = new AuctionsDataContext();
+            var categories = db.Auctions.Select(x => x.Category).Distinct();
+            ViewBag.Categories = categories.ToList();
+            return PartialView();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult SwitchView(string returnUrl, bool mobile = false)
+        {
+            HttpContext.ClearOverriddenBrowser();
+            HttpContext.SetOverriddenBrowser(mobile ? BrowserOverride.Mobile : BrowserOverride.Desktop);
+            return RedirectToAction(returnUrl);
+        }
+    }
+}
